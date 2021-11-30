97b28263ae3a30a5ee30f398ef340ef859f2b6fa
Bao Nguyen <chibaobguyen5585@gmail.com>
2021-11-30 22:59:45 +0700

    Chung thuc phia server

---
+++ backend/app/models/index.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const createUserModel = require("./user.model");
+
+const db = {};
+db.mongoose = mongoose;
+db.User = createUserModel(mongoose);
+
+module.exports = db;
